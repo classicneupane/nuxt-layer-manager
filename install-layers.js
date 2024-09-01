3357906ece7c413f320f494c7e9faf34abd0dfec
Classic Neupane <classicneupane@gmail.com>
2024-09-01 12:25:16 +0545

    feat: add script to clean layers before cloning

--- install-layers.js
+++ install-layers.js
@@ -1,27 +1,105 @@
 var fs = require('fs');
+const path = require("path")
 const { exec } = require("child_process")
+
+
+const logger = {
+    logs: [],
+    displayLog: function () {
+        const lastLog = this.logs[this.logs.length - 1];
+        if (lastLog.type === 'info') {
+            console.log(lastLog.message)
+        }
+        else if (lastLog.type === 'warn') {
+            console.warn(lastLog.message)
+        }
+        else if (lastLog.type === 'error') {
+            console.error(lastLog.message)
+        }
+    },
+
+    log: function (message) {
+        this.logs.push({
+            type: 'info',
+            message
+        })
+        this.displayLog()
+    },
+    error: function (message) {
+        this.logs.push({
+            type: 'error',
+            message
+        })
+        this.displayLog()
+    },
+    warn: function (message) {
+        this.logs.push({
+            type: 'warn',
+            message
+        })
+        this.displayLog()
+    }
+}
 
 /** @typedef {{ name: string, url: string }} LayerType */
 
 /**
  * 
- * @param {LayerType} layer 
+ * @param {string} name 
  */
-async function installLayer(layer) {
-    exec(`git clone ${layer.url}`)
+function removeLayerDir(name) {
+    return new Promise((resolve, reject) => {
+        if (!name.startsWith('layer-')) {
+            logger.warn(`Skipping: ${name}`)
+            return resolve()
+        }
+        const dirPath = path.join(__dirname, name)
+        fs.rmSync(dirPath, { force: true, recursive: true })
+    })
 }
 
-(async () => {
+function cleanLayer() {
+    const dirs = fs.readdirSync(__dirname)
+    for (const name of dirs) {
+        removeLayerDir(name)
+    }
+}
+
+/**
+ * 
+ * @param {LayerType} layer 
+ */
+function installLayer(layer) {
+    return new Promise((resolve, reject) => {
+        exec(`git clone ${layer.url}`, (e) => {
+            if(e){
+                reject(e)
+            }
+            else{
+                resolve(e)
+            }
+        })
+    })
+}
+
+/**
+ * 
+ * @returns {Array<LayerType>}
+ */
+function readLayerConfig() {
     var obj = JSON.parse(fs.readFileSync('layers.json', 'utf8'));
 
     if (!obj || !(obj?.layers)) {
         throw new Error("Invalid layers specification file format")
     }
 
-    const layers = obj.layers;
+    return obj.layers;
+}
 
+(async () => {
+    cleanLayer()
+    const layers = readLayerConfig()
     for (const layer of layers) {
-        const r = await installLayer(layer)
-        console.log(r)
+        await installLayer(layer)
     }
 })()
